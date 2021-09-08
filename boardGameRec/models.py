67e7cba97e267ce85c9dc71f22b3e94d41521848
adj5672 <adj5672@naver.com>
2021-09-08 13:10:41 +0900

    models.py

--- boardGameRec/models.py
+++ boardGameRec/models.py
@@ -1,33 +1,37 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Game(models.Model):
-    id = models.IntegerField()
-    thumbnail = models.CharField()
-    image = models.CharField()
-    name = models.CharField() # primary
-    alternate = models.TextField() # 리스트
+    id = models.IntegerField(primary_key=True)
+    thumbnail = models.CharField(max_length=100)
+    image = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)  # primary
+    alternate = models.TextField()  # 리스트
     description = models.TextField()
     year_published = models.IntegerField()
-    min_players = models.IntegerField()
-    max_players = models.IntegerField()
-    min_play_time = models.IntegerField()
-    max_play_time = models.IntegerField()
-    min_age = models.IntegerField()
-    category = models.TextField() # 리스트
-    play_type = models.TextField() # 리스트 # boardgamemechanic
-    series = models.TextField() # 리스트 # boardgameimplementation
-    designer = models.CharField() # boardgamedesigner
-    artist = models.TextField() # 리스트 # boardgameartist
-    publisher = models.TextField() # 리스트 # boardgamepublisher
-    users_rated = models.IntegerField()
-    average_rate = models.FloatField() # average
-    rank = models.IntegerField() # Board Game Rank
+    minPlayers = models.IntegerField()
+    maxPlayers = models.IntegerField()
+    minPlayTime = models.IntegerField()
+    maxPlayTime = models.IntegerField()
+    minAge = models.IntegerField()
+    category = models.TextField()  # 리스트
+    playType = models.TextField()  # 리스트 # boardgamemechanic
+    series = models.TextField()  # 리스트 # boardgameimplementation
+    designer = models.CharField(max_length=100)  # boardgamedesigner
+    artist = models.TextField()  # 리스트 # boardgameartist
+    publisher = models.TextField()  # 리스트 # boardgamepublisher
+    usersRated = models.IntegerField()
+    averageRate = models.FloatField()  # average
+    rank = models.IntegerField()  # Board Game Rank
 
 
 class Review(models.Model):
     user = models.CharField(max_length=50)
     rating = models.FloatField()
     comment = models.TextField()
-    game_id = models.ForeignKey(Game, on_delete=models.CASCADE)
-    game_name = models.CharField()
+    # gameId = models.ForeignKey(Game, on_delete=models.CASCADE)
+    gameName = models.CharField(max_length=100)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
