a0b3bc3d86ab60c1db44ce17be529c4ad9db71fb
WoosubLeee <zbxv1423@naver.com>
2021-09-09 15:57:00 +0900

    df_maker 일부 완료

--- boardGameRec/datas/df_maker.py
+++ boardGameRec/datas/df_maker.py
@@ -11,26 +11,67 @@ from pprint import pprint
 def make_dataframe():
     game_ids = fetch_game_ids()
     games = []
-    for id in game_ids[:50]:
+
+    # API에 데이터가 없을 수도 있는 column들
+    columns_check = {
+        'thumbnail': 'thumbnail',
+        'image': 'image',
+        'description': 'description',
+        'yearPublished': ['int', 'yearpublished'],
+        'minPlayers': ['int', 'maxplayers'],
+        'maxPlayers': ['int', 'maxplayers'],
+        'minPlayTime': ['int', 'minplaytime'],
+        'maxPlayTime': ['int', 'maxplaytime'],
+        'minAge': ['int', 'age'],
+        'category': ['list', 'boardgamecategory'],
+        'playType': ['list', 'boardgamemechanic'],
+        'series': ['list', 'boardgameimplementation'],
+        'designer': ['list', 'boardgamedesigner'],
+        'artist': ['list', 'boardgameartist'],
+        'publisher': ['list', 'boardgamepublisher']
+    }
+
+    for id in game_ids[:10]:
         url = f'https://www.boardgamegeek.com/xmlapi/boardgame/{id}'
         u = urlopen(url).read()
         doc = xmltodict.parse(u)
         game = doc['boardgames']['boardgame']
         
-        games.append({
+        data = {
             'id': id,
-            'thumbnail': game['thumbnail'],
-            'image': game['image'],
+            'thumbnail': '',
+            'image': '',
             'name': fetch_name(game['name']),
             'nameKor': fetch_korean_name(game['name']),
-            'description': game['description'],
-            'yearPublished': int(game['yearpublished']),
-            'minPlayers': int(game['minplayers']),
-            'maxPlayers': int(game['maxplayers']),
-            'minPlayTime': int(game['minplaytime']),
-            'maxPlayTime': int(game['maxplaytime']),
-            'minAge': int(game['minAge']),
-        })
+            'description': '',
+            'yearPublished': '',
+            'minPlayers': '',
+            'maxPlayers': '',
+            'minPlayTime': '',
+            'maxPlayTime': '',
+            'minAge': '',
+            'category': '',
+            'playType': '',
+            'series': '',
+            'designer': '',
+            'artist': '',
+            'publisher': ''
+        }
+
+        # try 문을 사용, api에 해당 항목이 있다면 data 추가
+        for key, val in columns_check.items():
+            try:
+                if type(val) is str:
+                    data[key] = game[val]
+                elif type(val) is list:
+                    if val[0] == 'int':
+                        data[key] = int(game[val[1]])
+                    else:
+                        data[key] = convert_to_list(game[val[1]])
+            except:
+                pass
+
+        games.append(data)
 
 
 def fetch_game_ids():
@@ -57,6 +98,16 @@ def fetch_korean_name(names):
                 return name
     except:
         pass
+
+
+def convert_to_list(dict_list):
+    result = []
+    if type(dict_list) is list:
+        for category in dict_list:
+            result.append(category['#text'])
+    else:
+        result.append(dict_list['#text'])
+    return str(result)
 
 
 def isHangul(text):
